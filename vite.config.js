6f63b78d6a6094d842967363e275331e0c661043
ianfhunter <ianfhunter@gmail.com>
2026-01-11 20:30:49 +0000

    vite

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
     plugins: [react()],
     server: {
         host: '0.0.0.0', // Listen on all network interfaces
-        allowedHosts: 'all' // Allow all hosts
+        allowedHosts: true // Allow all hosts
     }
   })
